81dacc791bbd8ac8a8056c1546b8b33d4c1be40b
Haukalauk <gaukur5@hotmail.com>
2018-10-09 22:46:02 +0000

    Skilaverkefni1

---
+++ js/main.js
@@ -0,0 +1,22 @@
+var svar1 = "Gledileg jól" + " og farsælt komandi ár";
+
+var svar2 = 5 * 4 / 2 + 3 -1;
+
+var Nótt = true;
+var Dagur = false;
+
+var svar3 = (Dagur&&Dagur)||!(Dagur&&Nótt);
+
+var svar4 = (5<6)>(8===8);
+
+var svar5 = (8<=10) || (6>=6)!==10;
+
+var svar6 = "hún er med True gildi. Hún er með = fyrir framan aboutKalli og síðan === svo hún fær sama True gildið"; 
+
+var svar7 = "hún er med True gildi. ! fyrir framan palli er godur strakur gefur tvi not gildi svo palli er vondur strakur og palli er vondur strakur"
+
+var svar8 = "not-not upphrópunarmerkin skila öllum breytum í upprunalegt gildi. a-false b-true c-false d-kalli"
+
+var svar9 = "Hún er True. Breytan er minni en 9"
+
+var svar10 = "#1 Zoey knight - Mel knive . #2"
